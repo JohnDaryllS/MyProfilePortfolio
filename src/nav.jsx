90d7a17d4714c64fd93dcca3312ff3d3e99cb4b8
John Daryll Sampilingan <johndaryllramos8@gmail.com>
2024-10-18 18:52:40 +0800

    first commit

---
+++ src/nav.jsx
@@ -0,0 +1,23 @@
+
+
+function Navigation () {
+    return(
+        <nav>
+            <div class="nav-content">
+            <div class="logo">
+                <a href="#">My Job Portfolio</a>
+            </div>
+            <ul class="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Achievements</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Resources</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            </div>
+        </nav>
+    );
+
+}
+
+export default Navigation
