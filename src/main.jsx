f5c8d9a14ddccc61aebf48e60b50c47f7787edd3
John Daryll Sampilingan <johndaryllramos8@gmail.com>
2024-10-18 22:19:18 +0800

    adding some styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Navigation from './nav.jsx'
-import Header from './header.jsx'
+import Header from './card.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
